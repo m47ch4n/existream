c562fa86701e8c4a3af97232cf635f52f9f78d49
M47CH4N <m47ch4n@gmail.com>
2019-04-18 12:53:28 +0900

    Add meta

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import { Helmet } from 'react-helmet'
+
 import './App.css'
 
 class App extends Component {
+  componentWillMount() {
+    window.prerenderReady = false
+  }
+  componentDidMount() {
+    window.prerenderReady = true
+  }
+
   render() {
     return (
       <div className="App">
+        <Helmet>
+          <meta name="og:title" content="existream" />
+          <meta name="og:description" content="Synchronize YouTube contents" />
+          <meta name="og:url" content={window.location.href} />
+          <meta name="twitter:card" content="summary" />
+        </Helmet>
         <header className="App-header">
           { this.props.children }
         </header>
